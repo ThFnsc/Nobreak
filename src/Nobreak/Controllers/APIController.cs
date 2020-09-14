4cf537d95ae7952c94dbcd0dd80bfa1df1dfdfec
Thiago Fonseca <thiagofnsc@outlook.com>
2020-09-14 02:06:26 -0300

    Memory cache (#3)
    Co-authored-by: Thiago Fonseca da Silveira <thiago.fonseca@umbler.com>

--- src/Nobreak/Controllers/APIController.cs
+++ src/Nobreak/Controllers/APIController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Nobreak.Controllers
 {
@@ -16,26 +17,29 @@ namespace Nobreak.Controllers
     [ApiController]
     public class APIController : ControllerBase
     {
-        private readonly CachedInfos _cachedInfos;
-        private readonly NobreakContext _context;
+        private static readonly string RECENT_VALUES_JSON_ENTRY = nameof(RECENT_VALUES_JSON_ENTRY);
+        private static readonly string UPTIME_JSON_ENTRY = nameof(UPTIME_JSON_ENTRY);
 
-        public APIController(CachedInfos cachedInfos, NobreakContext context)
+        private readonly NobreakContext _context;
+        private readonly IMemoryCache _memoryCache;
+
+        public APIController(NobreakContext context, IMemoryCache memoryCache)
         {
-            _cachedInfos = cachedInfos;
             _context = context;
+            _memoryCache = memoryCache;
         }
 
         [HttpGet("RecentValues")]
         public async Task<IActionResult> RecentValues() =>
-            Content(await _cachedInfos.GetAsync<string>(CachedInfos.ShortTermNobreakStatesJsonIdentifier), "application/json");
-
-        [HttpGet("AllValues")]
-        public async Task<IActionResult> AllValues() =>
-            Content(await _cachedInfos.GetAsync<string>(CachedInfos.LongTermNobreakStatesJsonIdentifier), "application/json");
+            Ok(await _memoryCache.GetAsync(RECENT_VALUES_JSON_ENTRY, async () =>
+            {
+                var since = DateTime.Now - TimeSpan.FromDays(1);
+                return (await _context.NobreakStates.Where(s => (s.Id % 30 == 0 || s.Id > _context.NobreakStates.Max(ss => ss.Id) - 30) && s.Timestamp >= since).OrderByDescending(s => s.Timestamp).ToListAsync()).ToJSON();
+            }, TimeSpan.FromSeconds(2)));
 
         [HttpGet("Uptime")]
         public async Task<IActionResult> Uptime() =>
-            Content(await _cachedInfos.GetAsync<string>(CachedInfos.UptimeReportJsonIdentifier), "application/json");
+            Ok((await _memoryCache.GetAsync(UPTIME_JSON_ENTRY, async () => await UptimeReport.Calculate(_context), TimeSpan.FromSeconds(2))).ToJSON());
 
         [HttpGet("ToggleOnPurpose/{id}")]
         [Authorize]
@@ -44,8 +48,8 @@ namespace Nobreak.Controllers
             var evt = await _context.NobreakStateChanges.SingleAsync(s => s.Id == id);
             evt.OnPurpose = !evt.OnPurpose;
             await _context.SaveChangesAsync();
-            _cachedInfos.Invalidate(CachedInfos.UptimeReportIdentifier);
-            _cachedInfos.Invalidate(CachedInfos.UptimeReportJsonIdentifier);
+            _memoryCache.Remove(RECENT_VALUES_JSON_ENTRY);
+            _memoryCache.Remove(UPTIME_JSON_ENTRY);
             return Ok();
         }
     }
---
+++ src/Nobreak/Extensions/MemoryCacheExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Caching.Memory
+{
+    public static class MemoryCacheExtensions
+    {
+        public static Task<T> GetAsync<T>(this IMemoryCache memoryCache, object identifier, Func<Task<T>> getValue, TimeSpan ttl) =>
+            memoryCache.TryGetValue(identifier, out object cached)
+                ? (Task<T>)cached
+                : memoryCache.Set(identifier, Task.Run(getValue), ttl);
+    }
+}
