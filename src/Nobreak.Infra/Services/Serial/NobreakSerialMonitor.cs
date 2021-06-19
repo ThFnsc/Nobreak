d8ab251ebc2f52544ac93bb23bdc3d5350bded9d
Thiago Fonseca da Silveira <thiago.fonseca@umbler.com>
2021-06-19 03:14:15 -0300

    Fix no port exception

--- src/Nobreak.Infra/Services/Serial/NobreakSerialMonitor.cs
+++ src/Nobreak.Infra/Services/Serial/NobreakSerialMonitor.cs
@@ -29,7 +29,7 @@ namespace Nobreak.Infra.Services.Serial
 
             var serialPorts = SerialPort.GetPortNames();
             _logger.LogInformation("Portas seriais encontradas: {SerialPorts}", string.Join(", ", serialPorts));
-            var chosenSerialPort = _appSettings.SerialPort ?? serialPorts.Last();
+            var chosenSerialPort = _appSettings.SerialPort ?? serialPorts.LastOrDefault();
             if (string.IsNullOrWhiteSpace(chosenSerialPort))
                 _logger.LogWarning("Nenhuma porta serial escolhida/encontrada");
             else
